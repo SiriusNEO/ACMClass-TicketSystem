868956c25229df0115f9737abd7d12f73f1756fc
SiriusNEO <1713833595@qq.com>
2021-08-15 15:20:46 +0800

    parse error will not quit now

---
+++ BackEnd/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/17138/Desktop/ACMClass-TicketSystem/BackEnd/src/main.cpp" "/mnt/c/Users/17138/Desktop/ACMClass-TicketSystem/BackEnd/CMakeFiles/code.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "db"
+  "lib"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
